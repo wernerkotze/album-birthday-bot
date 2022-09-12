50338750dcb97d14aa617613e74d738a6338cd30
wernerkotze <wernerkotze.rsa@gmail.com>
2022-09-12 22:02:07 +0200

    added header and todos endpoint

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,6 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
+
 origins = [
     "http://localhost:3000",
     "localhost:3000"
@@ -19,3 +30,7 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
 	return {"message": "Welcome to the best album birthday generator on the internet!"}
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+	return { "data": todos }
